d0b4410fa72c2681768f756ec0050fc4f4f627d6
Henri <henri-zhang@outlook.com>
2025-11-04 15:46:56 +0800

    add qx conf

--- qx/qq_url_redirect.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-    try{
-        window.location.href=getParams("pfurl");
-    }catch(err){
-        console.error(err);
-        window.location.href=decodeURIComponent(/pfurl\=(.*?)&+/.exec(window.location.href)[1])
-    }
-})();
